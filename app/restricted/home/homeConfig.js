5ec0fc8a2a86397ebed503dff1d982381a911794
Webbaroli <davide.macocchi@gmail.com>
2015-07-20 11:47:18 +0200

    Aggiunti dei commenti a home

--- app/restricted/home/homeConfig.js
+++ app/restricted/home/homeConfig.js
@@ -6,8 +6,16 @@ angular.module('homeModule', []) //dichiaro il modulo
 	$routeProvider.when(
 		'/home', 
 		{
+            //USA QUESTO TEMPLATE NELLA ng-view....
 			templateUrl: 'restricted/home/home.html',
+            //CON QUESTO CONTROLLER...
 			controller: 'homeController',
+            //MA PRIMA, risolvi questa variabile!
+            /*
+            NB se la variabile viene risolta OK, può essere utilizzata e iniettata come dipendeza nella funzione del controller
+            MA se la dipendenza non viene risolta viene emesso un evento $routeChangeError al $rootscope! se non intercettato
+            e gestito si ottiene una pagina bianca. (vedi app.js nel blocco .run())
+             */
 	        resolve: {
 	            authorization: function(userService){
 	                return userService.isAuthorized();
